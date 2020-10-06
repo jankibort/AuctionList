0711fec78cc7ed9cbc94bd61bbacb0f3cb052a0f
jankibort <jan.kibort@gmail.com>
2020-10-06 10:50:24 +0200

    another steps in configuration project

--- index.php
+++ index.php
@@ -1,12 +1,23 @@
-<html>
+<!doctype html>
+<html lang="pl">
 <head>
-
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+    <script src="scripts.js"></script>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <title>AuctionList</title>
 </head>
 <body>
-<?php
-echo "hello world";
-?>
+<div>
+<!--    TODO:do sth - there's nothing here yet-->
+</div>
 </body>
 </html>
-
 
