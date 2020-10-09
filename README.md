9d5619ee4abde6da794ad6700f734d7c1a64370b
jankibort <jan.kibort@gmail.com>
2020-10-09 10:56:58 +0200

    some simple docker configuration (nginx php pgsql)

--- README.md
+++ README.md
@@ -4,8 +4,10 @@ This project is a light version of listing service. All you need to do to explor
 - ##### edit .env
 - ##### cd build
 - ##### docker-compose up --build -d 
-- ##### edit etc/hosts file > add "127.0.0.1 auctionlist"
+- ##### edit etc/hosts file > add "127.0.0.1 dockerized"
+**From now on there is service dockerized:8080 aviable**
+###### WALK AROUND: 
+######- on steps 2,3,4: extract files from "build" straight to "AuctionList" directory and replace ${PROJECT_PATH} var with . in docker-compose.yml 
+######- on last step: edit nginx.conf > "server_name localhost"
+######- those steps will make service aviable on localhost:8080
 
-###### [WALK AROUND on steps 2,3,4: extract files from "build" straight to "AuctionList" directory and replace ${PROJECT_PATH} var with . in docker-compose.yml]
-
-**Since now there is service auctionlist:8080 aviable**
--- templates/index.php
+++ templates/index.php
@@ -1,27 +1,14 @@
-<!doctype html>
-<html lang="pl">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
-    <script src="scripts.js"></script>
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <title>AuctionList</title>
-</head>
-<body>
-<div>
-<!--    TODO:do sth - there's nothing here yet-->
-    afdsafsadfas
-    <?php
-    echo phpversion();
-    ?>
-</div>
-</body>
-</html>
+<?php
+    //vars:
+    $host = "postgresql";
+    $port = 5432;
+    $dbname = "data";
+    $user = "sm";
+    $password = "qwe123";
+    //db query vars
+    $conn = pg_connect("host=$host  port=$port  dbname=$dbname user=$user password=qwe123");
+
+    echo "Hello, it's php " . phpversion() . " version. You are " . ($conn ? 'connected' : 'not connected') . " to database";
+
+
 
